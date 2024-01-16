e34701ee53189eaf461340261d75dc7960f47695
juliovinnicius <50780080+juliovinnicius@users.noreply.github.com>
2024-01-16 15:33:02 -0300

    configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:list_firestore/home/presentation/pages/home_page.dart';
+import 'package:list_firestore/modules/home/presentation/pages/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
